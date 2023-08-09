80a4e57f3b9d1d3a868c711fc221e45dbf037e00
Kiet0903 <htk09032003@gmail.com>
2023-08-10 01:36:34 +0700

    day 6

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 
 import './App.css';
-import Greet from './component/greet';
-import Welcome from './component/Welcome';
-import Hello from './component/Hello';
-import Message from './component/Message';
-import Counter from './component/Counter';
-import FunctionClick from './component/FunctionClick';
-import ClassClick from './component/ClassClick';
-import EventBind from './component/EventBind';
-import ParentComponent from './component/ParentComponent';
-import UserGreeting from './component/UserGreeting';
-import NameList from './component/NameList';
-import Stylesheet from './component/Stylesheet';
-import Inline from './component/Inline';
-import './appStyle.css'
-import styles from './appStyle.module.css'
+import LifecycleA from './component/LifecycleA';
+// import Greet from './component/greet';
+// import Welcome from './component/Welcome';
+// import Hello from './component/Hello';
+// import Message from './component/Message';
+// import Counter from './component/Counter';
+// import FunctionClick from './component/FunctionClick';
+// import ClassClick from './component/ClassClick';
+// import EventBind from './component/EventBind';
+// import ParentComponent from './component/ParentComponent';
+// import UserGreeting from './component/UserGreeting';
+// import NameList from './component/NameList';
+// import Stylesheet from './component/Stylesheet';
+// import Inline from './component/Inline';
+// import './appStyle.css'
+// import styles from './appStyle.module.css'
+// import Form from './component/form';
 
 
 function App() {
   return (
     <div className="App">
 
-      <h1 className='error'>error</h1>
-      <h1 className={styles.success}>success</h1>
+      <LifecycleA/>
+
+     {/* <Form/> */}
+
+
+
+      {/* <h1 className='error'>error</h1>
+      <h1 className={styles.success}>success</h1> */}
 
 
       {/* <Stylesheet primary={true}/>
