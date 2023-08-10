93aa578975d2a3d322387fbf21f7d871c4ec66ab
Kiet0903 <htk09032003@gmail.com>
2023-08-10 15:06:24 +0700

    day 7

---
+++ src/component/RegComp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class RegComp extends Component {
+    
+  render() {
+    return (
+      <div>Regular Comp {this.props.name}</div>
+    )
+  }
+}
+
+export default RegComp
